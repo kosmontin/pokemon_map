7c48526cda93823f6d84d24f8801df6855bc3b5c
Konstantin Beloborodov <kosta.white@gmail.com>
2022-04-11 13:04:28 +0300

    register Pokemon model on admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,3 +5,7 @@ class Pokemon(models.Model):
     '''Покемон'''
     title = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name = 'Покемон'
+        verbose_name_plural = 'Покемоны'
+
