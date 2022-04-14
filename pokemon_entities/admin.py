0807bb88d28ede67a3cb4f2c0058c83c1e84748f
Konstantin Beloborodov <kosta.white@gmail.com>
2022-04-14 21:45:03 +0300

    add preview image to AdminPage

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Pokemon, PokemonEntity
 
 
-admin.site.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    fields = [
+        'title', 'title_en', 'title_jp',
+        'image', 'preview',
+        'description', 'evolve_from'
+    ]
+    readonly_fields = ['preview']
+
+    def preview(self, obj):
+        return mark_safe(
+            f'<img src="{obj.image.url}" style="max-height: 200px;">'
+        )
+
+admin.site.register(Pokemon, PokemonAdmin)
 admin.site.register(PokemonEntity)
